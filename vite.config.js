c860f57a6f9b72a021de915f0c5f22dc7a923a8d
Nishant Kedare <kedarenishant11111@gmail.com>
2025-10-11 18:10:00 +0530

    Installed Admin panel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ["resources/css/filament/admin/theme.css", "resources/js/app.js"],
+      refresh: [
+        "resources/views/**/*.blade.php",
+        "app/Http/Controllers/**/*.php",
+        "app/Livewire/**/*.php",
+        "app/Filament/**/*.php",
+        "app/Providers/Filament/AdminPanelProvider.php",
+        "routes/**/*.php",
+        "public/js/alpine.js"
+      ]
+    }),
+    tailwindcss()
+  ]
 });
